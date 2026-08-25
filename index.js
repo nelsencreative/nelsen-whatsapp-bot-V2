fa54d04b1005c6a86fdd926e07b762a738907664
nelsencreative <admin@nelsen.web.id>
2026-08-26 01:05:58 +0700

    Add Browsers import from @whiskeysockets/baileys

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const {
     useMultiFileAuthState,
     DisconnectReason,
     fetchLatestBaileysVersion,
-    delay
+    delay,
+    Browsers
 } = require('@whiskeysockets/baileys');
 const { Boom } = require('@hapi/boom');
 const P = require('pino');
